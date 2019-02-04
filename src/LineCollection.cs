4080fffe7226650413a187d69c25469899d273bb
JP Dillingham <jpdillingham@gmail.com>
2019-02-03 19:49:27 -0600

    add project, load lines into memory

---
+++ src/LineCollection.cs
@@ -0,0 +1,25 @@
+﻿namespace LazyCsvFile
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class LineCollection
+    {
+        public List<string> Headers { get; } = new List<string>();
+        public List<string> Lines { get; } = new List<string>();
+
+        public void LoadFrom(string file)
+        {
+            using (var reader = new StreamReader(file))
+            {
+                var headers = reader.ReadLine();
+                Headers.AddRange(headers.Split(','));
+
+                while (!reader.EndOfStream)
+                {
+                    Lines.Add(reader.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LazyCsvFile
+{
+    using System;
+
+    public class Program
+    {
+        static LineCollection lines { get; } = new LineCollection();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Reading file...");
+            lines.LoadFrom(@"c:\file.csv");
+            Console.WriteLine($"Done.");
+
+            Console.WriteLine($"Headers: {string.Join(", ", lines.Headers)}");
+
+            Console.ReadKey();
+        }
+    }
+}
